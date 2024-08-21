f11f1a0ef29cba1ff86f1209da43992deed75026
DJ Schleen <djschleen@gmail.com>
2024-08-21 22:44:15 +0000

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod models;
+
 use async_std::task;
 use clap::Parser;
 use colored::*;
@@ -9,9 +10,8 @@ use surf;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
-
 struct Args {
-    #[arg(required(true))]
+    #[arg(required = true)]
     sbom: String,
 }
 
@@ -30,29 +30,31 @@ fn main() {
         }
     };
 
-    let bom = Bom::parse_from_json_v1_5(file_contents.as_bytes()).expect("Failed to parse BOM");
+    let bom = match Bom::parse_from_json_v1_5(file_contents.as_bytes()) {
+        Ok(bom) => bom,
+        Err(_) => {
+            eprintln!("Failed to parse BOM");
+            return;
+        }
+    };
 
-    let validation_result = bom.validate();
-    if validation_result.passed() {
-        println!("* SBOM is valid")
-    } else {
-        println!("* Provided file {} is not a valid SBOM", &args.sbom);
+    if !bom.validate().passed() {
+        eprintln!("* Provided file {} is not a valid SBOM", &args.sbom);
         return;
     }
 
+    println!("* SBOM is valid");
     if let Some(serial_number) = &bom.serial_number {
         println!("* SBOM Serial Number: {}", serial_number);
     }
 
-    let rate_limit_ms = 100; // Rate limit in milliseconds
-    let fut = process_sbom(&bom, rate_limit_ms);
-    match task::block_on(fut) {
-        Ok(_) => println!("process_sbom completed successfully"),
-        Err(err) => eprintln!("Error in process_sbom: {}", err),
+    let rate_limit_ms = 100;
+    if let Err(err) = task::block_on(process_sbom(&bom, rate_limit_ms)) {
+        eprintln!("Error in process_sbom: {}", err);
     }
+    println!("{}", "DONE!".green().bold());
 }
 
-//Produces an ascii header for trustier and prints it to the command line
 fn print_ascii_header() {
     let header = r#"
   __               __  _       
@@ -72,39 +74,40 @@ async fn process_sbom(
     bom: &Bom,
     rate_limit_ms: u64,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let mut collected_purls: Vec<String> = Vec::new();
-
-    if let Some(components) = &bom.components {
-        collected_purls = components
+    let mut collected_purls = if let Some(components) = &bom.components {
+        components
             .0
             .iter()
             .filter_map(|component| component.purl.as_ref().map(|purl| purl.to_string()))
-            .collect();
-    }
+            .collect::<Vec<_>>()
+    } else {
+        Vec::new()
+    };
 
     let original_count = collected_purls.len();
-
     filter_purls(&mut collected_purls);
 
     if collected_purls.len() < original_count {
         println!(
-            "{}",
-            r"* trustypkg.dev only supports the following ecosystems: pypi, npm, crates, maven, go"
+            "* {}",
+            r"trustypkg.dev only supports the following ecosystems: pypi, npm, crates, maven, go"
                 .red()
         );
         println!(
             r"* Removed {} out of {} detected Purls in the SBOM",
             original_count - collected_purls.len(),
             original_count
-        )
+        );
     }
 
-    println!("* Number of Purls: {}\n", collected_purls.len());
+    if collected_purls.len() != 0 {
+        println!("* Processing {} Purls...\n", collected_purls.len());
+    } else {
+        println!("* Nothing to do...\n")
+    }
 
-    // Call fetch_purl_bodies
     let bodies = fetch_purl_bodies(&collected_purls, rate_limit_ms).await?;
 
-    // Process the fetched bodies
     for (i, body) in bodies.iter().enumerate() {
         println!("PURL {}: {}", i, body);
     }
@@ -113,7 +116,7 @@ async fn process_sbom(
 }
 
 async fn fetch_purl_bodies(
-    purls: &Vec<String>,
+    purls: &[String],
     rate_limit_ms: u64,
 ) -> Result<Vec<String>, Box<dyn std::error::Error + Send + Sync + 'static>> {
     let mut bodies = Vec::new();
@@ -122,23 +125,19 @@ async fn fetch_purl_bodies(
         match PackageUrl::from_str(p) {
             Ok(purl) => {
                 let url = format!(
-                    r"https://api.trustypkg.dev/v2/pkg?package_name={}&package_type={}",
+                    "https://api.trustypkg.dev/v2/pkg?package_name={}&package_type={}",
                     purl.name(),
                     purl.ty()
                 );
 
                 println!("* Fetching trust information for {}:", p);
 
-                let mut response = surf::get(url).await?;
-                let body = response.body_string().await?;
+                let body = surf::get(url).await?.body_string().await?;
                 bodies.push(body);
 
-                // Rate limiting
                 task::sleep(std::time::Duration::from_millis(rate_limit_ms)).await;
             }
-            Err(err) => {
-                eprintln!("Error parsing PURL: {}", err);
-            }
+            Err(err) => eprintln!("Error parsing PURL: {}", err),
         }
     }
 
@@ -149,10 +148,8 @@ fn filter_purls(collected_purls: &mut Vec<String>) {
     let allowed_types = ["pypi", "npm", "crates", "maven", "go"];
 
     collected_purls.retain(|purl_str| {
-        if let Ok(purl) = PackageUrl::from_str(purl_str) {
-            allowed_types.contains(&purl.ty().to_string().as_str())
-        } else {
-            false
-        }
+        PackageUrl::from_str(purl_str)
+            .map(|purl| allowed_types.contains(&purl.ty()))
+            .unwrap_or(false)
     });
 }
